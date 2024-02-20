ce2cb3b01f77fecf22a2ac2b68097a14674f3ffa
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-21 01:10:42 +0530

    add cal pg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Calculetor from './views/calForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/cal' element={<Calculetor />}/>
+        <Route index element={<Calculetor />}/>
       </Routes>
     </BrowserRouter>
   );
