980632fc5163ebf43b33e8a15a9d0ba8e7bd26d3
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-23 23:40:29 +0530

    add container

--- src/views/calForm.jsx
+++ src/views/calForm.jsx
@@ -1,11 +1,15 @@
 import Navbar from '../component/nav_bar.jsx';
 const Calculator = () => {
     return (
-        <>
-            <div className="main-container">
-                <Navbar />
-            </div>
-        </>
+        <div className="main-container">
+            <Navbar />
+            <section className='body'>
+                <div className="calculator-container">
+
+                </div>
+            </section>
+        </div>
+
     );
 }
 
