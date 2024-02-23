347f83567b691f95d6f2d26859f5f9ae49e87e5b
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-24 01:28:53 +0530

    row modi

--- src/component/row/row.jsx
+++ src/component/row/row.jsx
@@ -2,7 +2,7 @@ import Btn from "../btns/btn";
 
 const Row = ({ vals, col, size, spc }) => {
     return (
-        <div class={`row  column-gap-3 ${spc}`}>
+        <div className={`row  column-gap-3 ${spc}`}>
             {vals.map((val, index) => (
                 <Btn key={index} colour={col[index]} val={val} spc={size[index]} />
             ))}
