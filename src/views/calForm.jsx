e893e4bf92c84e72ecb29656ea5c87d63a631305
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-24 01:32:24 +0530

    some change

--- src/views/calForm.jsx
+++ src/views/calForm.jsx
@@ -7,7 +7,7 @@ const Calculator = () => {
         <div className="main-container">
             <Navbar />
             <section className='body d-flex justify-content-center align-items-center'>
-                <section id="cal" className="border border-light rounded-5 d-flex flex-column justify-content-around shadow-lg p-3 mb-5 ">
+                <section className="cal border border-light rounded-5 d-flex flex-column justify-content-around shadow-lg p-3 mb-5 ">
 
                     <div className="w-100 border-bottom border-danger d-flex justify-content-end align-items-end">
                         <h1 id="main_num" className="me-4 mb-4 h1 display-1 ">0</h1>
