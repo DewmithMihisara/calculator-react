0367ca41851ce3552556f940d71233bbe521fb8c
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-24 01:28:44 +0530

    modi in mid row

--- src/component/row/mid_row.jsx
+++ src/component/row/mid_row.jsx
@@ -2,7 +2,7 @@ import Btn from "../btns/btn";
 
 const MidRow = ({ vals , sim}) => {
     return (
-        <div class="row column-gap-3 mt-3">
+        <div className="row column-gap-3 mt-3">
             {vals.map(val => (
                 <Btn colour={"dark"} val={val} spc={"col"}/>
             ))}
