fbdbaefb6d468d8a8ad4e92b67f5df7b7c893500
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-23 23:58:20 +0530

    make header

--- src/views/calForm.jsx
+++ src/views/calForm.jsx
@@ -3,10 +3,19 @@ const Calculator = () => {
     return (
         <div className="main-container">
             <Navbar />
-            <section className='body'>
-                <div className="calculator-container">
+            <section className='body d-flex justify-content-center align-items-center'>
+                <section id="cal" className="border border-light rounded-5 d-flex flex-column justify-content-around shadow-lg p-3 mb-5 ">
 
-                </div>
+                    <div className="w-100 border-bottom border-danger d-flex justify-content-end align-items-end">
+                        <h1 id="main_num" className="me-4 mb-4 h1 display-1 ">0</h1>
+                    </div>
+
+                    <div className="w-100 ">
+                        <div className="container text-center row-gap-3 text-light">
+                            
+                        </div>
+                    </div>
+                </section>
             </section>
         </div>
 
