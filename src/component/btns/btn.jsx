533b3cf536591cf025fefae4a6bbe511e7f8493f
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-24 01:28:33 +0530

    some modification in btn

--- src/component/btns/btn.jsx
+++ src/component/btns/btn.jsx
@@ -1,7 +1,7 @@
 const Btn=({colour,val,spc}) =>{
     return(
         <>
-            <button type="button" class={`btn btn-outline-${colour} ${spc} rounded-5 d-flex justify-content-center align-items-center`}>{val}</button>
+            <button type="button" className={`btn btn-outline-${colour} ${spc} rounded-5 d-flex justify-content-center align-items-center`}>{val}</button>
         </>
     )
 }
